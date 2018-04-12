9d4271ddbe8e852065a92f7de23678abfa7994a4
mkirkpatrick <mtraviskirkpatrick@gmail.com>
2018-04-11 22:19:15 -0500

    more git testing

---
+++ Assets/Scripts/Environment/Chunk_gameobj.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk_gameobj : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
