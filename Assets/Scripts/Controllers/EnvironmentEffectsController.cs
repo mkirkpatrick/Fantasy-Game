eb67a8c8579b395232ffd1bb8256fa0894607eac
mkirkpatrick <mtraviskirkpatrick@gmail.com>
2018-05-01 02:10:18 -0500

    mesh combining

--- Assets/Scripts/Controllers/EnvironmentEffectsController.cs
+++ Assets/Scripts/Controllers/EnvironmentEffectsController.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class EnvironmentEffectsController : MonoBehaviour {
 
+    public Camera mainCamera;
+
+    public GameObject sunObj;
+    public Color sunColor;
+
+    public GameObject moonObj;
+    public Color moonnColor;
+
+    //Skybox Stuff
+    public Color skyColor;
+    public Color equatorColor;
+    public Color groundColor;
+
 	// Use this for initialization
 	void Start () {
 		
--- Assets/Scripts/Environment/Chunk_gameobj.cs
+++ Assets/Scripts/Environment/Chunk_gameobj.cs
@@ -6,20 +6,57 @@ public class Chunk_gameobj : MonoBehaviour {
 
     public Chunk chunkData;
 
+    public GameObject groundPrefab;
+    public Material groundMaterial;
+    public List<GameObject> groundPieces;
+
 	// Use this for initialization
 	void Start () {
-        foreach (Transform ground in transform) {
-            if (ground.name == "GroundPiece")
-                ground.gameObject.AddComponent<MeshCollider>();
-        }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+        BuildGroundMeshes();
+        BuildColliders();
 	}
 
-    void OnDrawGizmosSelected() {
-        //Gizmos.DrawCube(transform.position, new Vector3(100, 5, 100));
+
+    private void BuildGroundMeshes() {
+        int vertexCount = 0;
+        List<GameObject> groundArray = new List<GameObject>();
+
+        foreach (Transform ground in transform) { 
+            if (ground.name == "GroundPiece") {
+                if (vertexCount < 50000) {
+                    groundArray.Add(ground.gameObject);
+                    vertexCount += ground.gameObject.GetComponent<MeshFilter>().mesh.vertexCount;
+                }    
+                else {
+                    CombineMeshes(groundArray.ToArray());
+                    vertexCount = 0;
+                    groundArray = new List<GameObject>();
+                }
+            }
+        }
+        if (groundArray.Count > 0)
+            CombineMeshes(groundArray.ToArray());
+    }
+    private void BuildColliders() {
+        foreach (GameObject ground in groundPieces) {
+            ground.AddComponent<MeshCollider>();
+        }
+    }
+
+    private void CombineMeshes(GameObject[] array) {
+        GameObject newGround = Instantiate(groundPrefab, transform);
+        CombineInstance[] combine = new CombineInstance[array.Length];
+
+        for (int i = 0; i < array.Length; i++) {
+            combine[i].mesh = array[i].GetComponent<MeshFilter>().sharedMesh;
+            combine[i].transform = array[i].transform.localToWorldMatrix;
+            Destroy(array[i]);
+        }
+
+        newGround.GetComponent<MeshFilter>().mesh = new Mesh();
+        newGround.GetComponent<MeshFilter>().mesh.CombineMeshes(combine);
+        newGround.GetComponent<MeshRenderer>().material = groundMaterial;
+
+        groundPieces.Add(newGround);
     }
 }
