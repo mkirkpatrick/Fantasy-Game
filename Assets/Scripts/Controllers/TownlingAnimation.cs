9080b00899ea4bcfbd9c49bf06d0839591562888
Caleb Masters <caleb.masters@endurance.com>
2018-06-10 20:47:12 -0700

    This demonstrates how the blend tree w/ the params coming from the script should work. The character was above ground and happy until I added the blend tree. Something about the idle animation I think is causing the issue.

---
+++ Assets/Scripts/Controllers/TownlingAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TownlingAnimation : MonoBehaviour {
+    public Animator Anim;
+    private float VerticalSpeed;
+	// Use this for initialization
+	void Start () {
+        Anim = GetComponent<Animator>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Anim.SetFloat("VerticalSpeed", moveVertical);
+	}
+}
